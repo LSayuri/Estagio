1afd26400e5c4d1bbc161e71973a6a9a0b45be33
cacocacaio <ccnsassino1998@hotmail.com>
2016-10-10 11:40:27 -0300

    sakhedbih
    aisfhaifh

--- js/meuscript.js
+++ js/meuscript.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
 	
-	
+	// ADD SLIDEDOWN ANIMATION TO DROPDOWN //
+	$('.dropdown').on('show.bs.dropdown', function(e){
+		
+		$(this).find('.dropdown-menu').first().stop(false, true).slideDown();
+	});
+
+	// ADD SLIDEUP ANIMATION TO DROPDOWN //
+	$('.dropdown').on('hide.bs.dropdown', function(e){
+		
+		$(this).find('.dropdown-menu').hide();
+	});
 });
